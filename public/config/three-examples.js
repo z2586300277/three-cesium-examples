154f1e08c65daa61c2f1e901196ead681e950b6f
zhangjingguang <2586300277@qq.com>
2024-07-11 10:53:38 +0800

    modelBlendShader

--- public/config/three-examples.js
+++ public/config/three-examples.js
@@ -70,6 +70,18 @@ export default [
         name: '着色器',
         children: [
             {
+                id: 'modelBlendShader',
+                name: '模型混合着色器',
+                author: 'z2586300277',
+                codeUrl: '/three-cesium-examples/public/threeExamples/shader/modelBlendShader.js',
+                image: '/three-cesium-examples/public/threeExamples/shader/modelBlendShader.jpg',
+                meta: {
+                    title: '模型混合着色器',
+                    keywords: 'three.js,模型混合着色器',
+                    description: '使用three.js模型混合着色器'
+                }
+            },
+            {
                 id: 'fenceShader',
                 name: '围栏着色器',
                 author: 'flowers-10',
