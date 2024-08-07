c2f16d868a1a6b53bebc6df2c282aef73a3b05a2
zhangjingguang <2586300277@qq.com>
2024-08-07 13:47:04 +0800

    chore: template

--- vite.config.js
+++ vite.config.js
@@ -1,60 +1,14 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-
-  plugins: [
-
-    {
-      transformIndexHtml: html => html.replace(
-        /<head>/,
-        `<head><script type="module" src="/three-cesium-examples/public/config/config.js"></script>`
-      )
-    },
-    
-    vue()
-
-  ],
-
-  define: {
-
-    __SITE_URLS__: {
-
-      gitee: 'https://gitee.com/giser2017/three-cesium-examples',
-
-      github: 'https://github.com/z2586300277/three-cesium-examples',
-
-      web: "http://threelab.cn/"
-
-    },
-
-  },
-
-  build: {
-
-    outDir: 'public',
-
-    emptyOutDir: false, // 不删除输出目录中的旧资源
-
-    target: 'es2015',
-
-    chunkSizeWarningLimit: 999999, // 最大打包体积
-
-    assetsInlineLimit: 0, // 资源内联限制
-
-    copyPublicDir: false,
-
-    cssCodeSplit: false, // css代码分割
-
-  },
 
   base: '/three-cesium-examples/public',
 
   server: {
 
-    port: 3344,
+    port: 8080,
 
-    open: true,
+    open: 'public/index.html',
 
     host: '0.0.0.0'
 
