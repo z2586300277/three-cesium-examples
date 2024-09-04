d779b3e5515c49c1aff3a5cd71d3123a4e79d3a2
zhangjingguang <2586300277@qq.com>
2024-09-04 14:39:58 +0800

    change: links

--- config/links.js
+++ config/links.js
@@ -3,24 +3,51 @@ import { HOST } from "./host.js"
 // 企业可根据自己的需求修改url 成自己的导航地址 链接
 export default {
     site: {
-        name: 'THREE.JS',
+        name: 'Three.js',
         url: 'https://threejs.org/',
         logo: HOST + 'files/site/logo.svg',
         footName: '- Home',
         footLink: HOST
     },
-    links : [
+    links: [
         {
-            name: '🏡Home',
-            url: 'https://threejs.org/'
+            name: '📦Three',
+            children: [
+                {
+                    name: '🏡Home',
+                    url: 'https://threejs.org/'
+                },
+                {
+                    name: '📚Documents',
+                    url: 'https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene'
+                },
+                {
+                    name: '🐘Examples',
+                    url: 'https://threejs.org/examples/#webgl_animation_keyframes',
+                },
+            ]
         },
+        
         {
-            name: '📚Documents',
-            url: 'https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene'
-        },
-        {
-            name: '🐘Examples',
-            url: 'https://threejs.org/examples/#webgl_animation_keyframes',
+            name: '🌐Cesium',
+            children: [
+                {
+                    name: '🏡Home',
+                    url: 'https://cesium.com/cesiumjs/'
+                },
+                {
+                    name: '🏠中文网',
+                    url: 'http://cesium.xin/'
+                },
+                {
+                    name: '📚Documents',
+                    url: 'https://cesium.com/docs/cesiumjs-ref-doc/'
+                },
+                {
+                    name: '🐘Examples',
+                    url: 'https://sandcastle.cesium.com/'
+                },
+            ]
         },
         {
             name: '🍃Open Source',
