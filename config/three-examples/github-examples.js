b19bb8444817fd383d0be3811f63e47b49f1ca34
zhangjingguang <2586300277@qq.com>
2024-11-03 10:56:29 +0800

    feat: vr hall

--- config/three-examples/github-examples.js
+++ config/three-examples/github-examples.js
@@ -8,5 +8,13 @@ export default [
         githubUrl: 'https://github.com/martinRenou/threejs-water',
         openUrl: 'https://martinrenou.github.io/threejs-water/',
         image: HOST + 'threeExamples/openSource/realWater.jpg',
+    },
+    {
+        id: 'vrHall',
+        author: 'AUTO',
+        name: 'VR展厅',
+        githubUrl: 'https://github.com/mtsee/vr-hall',
+        openUrl: 'http://test4.h5ds.com/',
+        image: HOST + 'files/site/logo.svg',
     }
 ]
