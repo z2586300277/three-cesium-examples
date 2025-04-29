573bfe2e83a4f7720037e86e9596c98d836c25af
zhangjingguang <2586300277@qq.com>
2025-04-29 09:36:22 +0800

    feat: 3dallery

--- config/github-examples.js
+++ config/github-examples.js
@@ -851,6 +851,15 @@ export default [
                 githubUrl: 'https://github.com/GitHubJackson/autopilot',
                 image: HOST + 'threeExamples/openSource/autopilot.jpg'
             },
+            {
+                id: 'steve245270533Gallery',
+                name: '3D画廊',
+                name_en: '3D Gallery',
+                author: 'AUTO',
+                openUrl: 'https://steve245270533.github.io/gallery/',
+                githubUrl: 'https://github.com/Steve245270533/gallery',
+                image: HOST + 'threeExamples/openSource/steve245270533Gallery.jpg'
+            },
 
             {
                 id: 'change',
