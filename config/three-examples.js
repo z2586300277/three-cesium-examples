f0ce84990e1b2bc3ff473a5fa9cbbedfa2a5b8cd
zhangjingguang <2586300277@qq.com>
2025-05-22 09:47:16 +0800

    chore: text update

--- config/three-examples.js
+++ config/three-examples.js
@@ -26,7 +26,7 @@ export default [
                 tag: TEXTS['推荐'],
                 tip: 'Cesium2024年最新demos，200多个原生示例：自定义纹理、地形开挖、可视域分析、洪水淹没、缓冲分析、日照分析、方量分析、通透分析、剖面、单体化、视频融合、视频投射、雨雪雾、天空盒、场景出图、体积云、闪电、视锥体、雷达扫描、水波纹、动态扩散点、电子围栏、粒子效果、物理引擎、克里金、海流、大数据可视化、坐标转换、相机、飞行漫游、瓦片加载、xyz、tms、wms、wmts、天地图、高德、百度、wkt、shp、geojson、pbf、地形、entity、模型、海量数据、Primitive、gif、聚合、czml、字体图标、运动、3Dtiles、模型偏移、等等。。',
                 author: 'jiawanlong',
-                name: 'Cesium案例',
+                name: 'Cesium案例 - 点⭐',
                 name_en: 'Cesium Examples',
                 openUrl: 'https://jiawanlong.github.io/Cesium-Examples/examples/cesiumEx/examples.html',
                 githubUrl: 'https://github.com/jiawanlong/Cesium-Examples',
